b01e80de4e2ee6b62b9e8e6d3827dc1988bdc82c
JTW08 <walkerjordan08@gmail.com>
2026-03-21 22:47:11 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# HealScape 20260322_0040
+# HealScape 20260322_0242
 https://jtw08.github.io/Healscape/ | https://healscape.netlify.app
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// HealScape Service Worker - build 20260322_0040
-const CACHE_NAME = 'healscape-20260322_0040';
+// HealScape Service Worker - build 20260322_0242
+const CACHE_NAME = 'healscape-20260322_0242';
 const ASSETS = ['/Healscape/','/Healscape/index.html','/Healscape/manifest.json',
   '/Healscape/icon-192.png','/Healscape/icon-512.png',
   '/Healscape/milo-actions.png','/Healscape/milo-eat-carrot.png',
